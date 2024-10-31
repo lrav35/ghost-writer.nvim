9de016a04d5b406abf530f9576d51050120eaf0f
Luc Ravenelle <lrav35@gmail.com>
2024-10-31 16:24:33 -0400

    adding window pop up and playing around with the buffer.. doesnt quite work correctly

--- lua/ghost-writer/init.lua
+++ lua/ghost-writer/init.lua
@@ -27,8 +27,40 @@ function M.make_request(tokens)
 end
 
 function M.run()
-	print("hello from the plugin!")
-	M.make_request("hello world!")
+	M.open_chat_window()
+	M.make_request()
+end
+
+function M.open_chat_window()
+	local buffer = vim.api.nvim_create_buf(false, true)
+	local start_message = "hello, how can I assist you?"
+
+	vim.cmd("75vsplit")
+	local win = vim.api.nvim_get_current_win()
+	vim.api.nvim_win_set_buf(win, buffer)
+
+	vim.bo[buffer].buftype = "nofile"
+	vim.bo[buffer].bufhidden = "wipe"
+	vim.bo[buffer].swapfile = false
+	vim.bo[buffer].filetype = "markdown"
+
+	vim.wo[win].relativenumber = false
+
+	vim.api.nvim_buf_set_lines(buffer, 0, -1, false, { start_message })
+
+	vim.api.nvim_create_autocmd("InsertEnter", {
+
+		group = vim.api.nvim_create_augroup("NotePanel", { clear = true }),
+		callback = function()
+			-- TODO: this doesnt really work the way i want it to, getting invalid buffer id warnings
+			local lines = vim.api.nvim_buf_get_lines(buffer, 0, -1, false)
+			if #lines == 1 and lines[1] == start_message then
+				vim.api.nvim_buf_set_lines(buffer, 0, -1, false, { "" })
+			end
+		end,
+	})
+
+	vim.api.nvim_set_current_win(win)
 end
 
 function M.setup()
