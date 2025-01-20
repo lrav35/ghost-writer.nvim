25ff73074c6b983de6d43d8cd86e6cc0d7534024
coins <lrav35@gmail.com>
2025-01-19 22:14:00 -0500

    refactor - pushing broken

--- lua/ghost-writer/init.lua
+++ lua/ghost-writer/init.lua
@@ -1,4 +1,5 @@
 local M = {}
+local defaults = {}
 local Job = require("plenary.job")
 
 local helpful_prompt =
@@ -13,10 +14,12 @@ local function get_api_key(name)
 end
 
 local function write_debug(message)
-	local debug_file = io.open("debug.log", "a")
-	if debug_file then
-		debug_file:write(os.date() .. " - " .. message .. "\n")
-		debug_file:close()
+	if M.config.debug then
+		local debug_file = io.open("debug.log", "a")
+		if debug_file then
+			debug_file:write(os.date() .. " - " .. message .. "\n")
+			debug_file:close()
+		end
 	end
 end
 
@@ -57,7 +60,7 @@ local function waiting(buf)
 	return timer
 end
 
-function M.parse_and_output_message(buf, result)
+local function parse_and_output_message(buf, result)
 	vim.schedule(function()
 		if not vim.api.nvim_buf_is_valid(buf) then
 			return
@@ -93,7 +96,6 @@ local anthropic_opts = {
 	url = "https://api.anthropic.com/v1/messages",
 	model = "claude-3-5-sonnet-20241022",
 	target_state = "content_block_delta",
-
 	api_key_name = "ANTHROPIC_API_KEY",
 	system_prompt = helpful_prompt,
 	replace = false,
@@ -152,7 +154,7 @@ function M.handle_stream_data(opts)
 
 		local success, json = pcall(vim.json.decode, stream)
 		if success and json.delta and json.delta.text then
-			M.parse_and_output_message(buf, json.delta.text)
+			parse_and_output_message(buf, json.delta.text)
 		end
 	end
 end
@@ -161,6 +163,7 @@ M.providers = {
 	anthropic = {
 		target_state = "content_block_delta",
 	},
+
 	-- Example for another provider
 	-- openai = {
 	--     target_state = "data", -- OpenAI-specific state
@@ -188,10 +191,13 @@ M.providers = {
 -- 	end
 -- end
 
-local group = vim.api.nvim_create_augroup("sup", { clear = true })
+local group = vim.api.nvim_create_augroup("LLM", { clear = true })
 local active_job = nil
 
-function M.make_request(tokens, opts, curl_args_fn, buf)
+function M.make_request(tokens, curl_args_fn, buf)
+	local provider = M.config.default
+	local provider_opts = M.config.providers[provider]
+
 	vim.api.nvim_clear_autocmds({ group = group })
 	local curr_event_state = nil
 	local waiting_task = waiting(buf)
@@ -201,7 +207,7 @@ function M.make_request(tokens, opts, curl_args_fn, buf)
 		active_job = nil
 	end
 
-	local local_args = curl_args_fn(opts, tokens)
+	local local_args = curl_args_fn(provider_opts, tokens)
 
 	local function handle_stdout(data, curr_state, buffer, task, handle_data_fn)
 		if not data then
@@ -225,13 +231,8 @@ function M.make_request(tokens, opts, curl_args_fn, buf)
 		command = "curl",
 		args = local_args,
 		on_stdout = function(_, data)
-			curr_event_state = handle_stdout(
-				data,
-				curr_event_state,
-				buf,
-				waiting_task,
-				M.handle_stream_data(opts) -- or whatever handler you want to use
-			)
+			curr_event_state =
+				handle_stdout(data, curr_event_state, buf, waiting_task, M.handle_stream_data(provider_opts))
 		end,
 		on_stderr = function(_, data)
 			write_debug("STDERR: " .. vim.inspect(data))
@@ -247,7 +248,7 @@ function M.make_request(tokens, opts, curl_args_fn, buf)
 
 	vim.api.nvim_create_autocmd("User", {
 		group = group,
-		pattern = "model_escape_fn",
+		pattern = M.config.keymaps.escape.pattern,
 		callback = function()
 			if active_job then
 				active_job:shutdown()
@@ -257,7 +258,13 @@ function M.make_request(tokens, opts, curl_args_fn, buf)
 		end,
 	})
 
-	vim.api.nvim_set_keymap("n", "<Esc>", ":doautocmd User model_escape_fn<CR>", { noremap = true, silent = true })
+	vim.keymap.set(
+		"n",
+		M.config.keymaps.escape.key,
+		string.format(":doautocmd User %s<CR>", M.config.keymaps.escape.pattern),
+		{ noremap = true, silent = true }
+	)
+
 	return active_job
 end
 
@@ -278,13 +285,13 @@ function M.state_manager()
 		return window
 	end
 
-	local function setup_autocmd(buffer, start_message)
+	local function setup_autocmd(buffer)
 		vim.api.nvim_create_autocmd("InsertEnter", {
 			group = vim.api.nvim_create_augroup("NotePanel", { clear = true }),
 			callback = function()
 				if vim.api.nvim_get_current_buf() == buffer then
 					local lines = vim.api.nvim_buf_get_lines(buffer, 0, -1, false)
-					if #lines == 1 and lines[1] == start_message then
+					if #lines == 1 and lines[1] == M.config.ui.default_message then
 						vim.api.nvim_buf_set_lines(buffer, 0, -1, false, { "" })
 					end
 				end
@@ -303,17 +310,15 @@ function M.state_manager()
 
 	local function setup_keybindings(buffer)
 		local opts = { noremap = true, silent = true, buffer = buffer }
-		vim.keymap.set("n", "<A-h>", function()
+		vim.keymap.set("n", M.config.keymaps.buffer.resize_left.key, function()
 			resize_window("left")
 		end, opts)
-		vim.keymap.set("n", "<A-l>", function()
+		vim.keymap.set("n", M.config.keymaps.buffer.resize_right.key, function()
 			resize_window("right")
 		end, opts)
 	end
 
 	local function create_win_and_buf()
-		local start_message = "hello, how can I assist you?"
-
 		if context then
 			return context
 		end
@@ -321,14 +326,14 @@ function M.state_manager()
 		local buffer = setup_buffer(vim.api.nvim_create_buf(false, true))
 
 		-- window width
-		vim.cmd(70 .. "vsplit")
+		vim.cmd(M.config.ui.window_width .. "vsplit")
 		local window = setup_window(vim.api.nvim_get_current_win())
 
 		vim.api.nvim_win_set_buf(window, buffer)
 
-		vim.api.nvim_buf_set_lines(buffer, 0, -1, false, { start_message })
+		vim.api.nvim_buf_set_lines(buffer, 0, -1, false, { M.config.ui.default_message })
 
-		setup_autocmd(buffer, start_message)
+		setup_autocmd(buffer)
 		setup_keybindings(buffer)
 		context = { buf = buffer, win = window }
 		return context
@@ -347,7 +352,7 @@ function M.state_manager()
 		if context and vim.api.nvim_buf_is_valid(context.buf) then
 			local lines = vim.api.nvim_buf_get_lines(context.buf, 0, -1, false)
 			local message = table.concat(lines, "\n")
-			M.make_request(message, anthropic_opts, M.get_anthropic_specific_args, context.buf)
+			M.make_request(message, M.get_anthropic_specific_args, context.buf)
 		end
 	end
 
@@ -366,24 +371,32 @@ end
 
 -- [[
 -- TODO:
--- fix new line bug - misses some of them (DONE i think)
--- Queue solution to avoid race conditions? it only misses an "I" in "I'll" idk
--- scroll page as streaming response comes in DONE
--- keybindings to adjust split size DONE
--- create toggle ability that saves all prior context
--- fix spinngin loader - it doesnt work right unless you are at the last line DONE
 -- clean up funcions specific to anthropic and make more generic WIP
 -- add capability for other apis
 -- move most config based code to setup in nvim config
+-- fix this bug:
+-- Error executing vim.schedule lua callback: ...ode/personal/ghost-writer.nvim/lua/ghost-writer/init.lua:78: 'replacement string' item contains newlines
+-- stack traceback:
+--         [C]: in function 'nvim_buf_set_lines'
+--         ...ode/personal/ghost-writer.nvim/lua/ghost-writer/init.lua:78: in function <...ode/personal/ghost-writer.nvim/lua/ghost-writer/init.lua:61>
 --
 -- ]]
 
-function M.setup()
+function M.setup(opts)
+	M.config = vim.tbl_deep_extend("force", defaults, opts or {})
+	print(vim.inspect(M.config))
 	local manager = M.state_manager()
 
-	vim.keymap.set("n", "<leader>wo", manager.open, { desc = "[W]indow [O]pen Chat", noremap = true, silent = true })
-	vim.keymap.set("n", "<leader>we", manager.exit, { desc = "[W]indow [E]xit", noremap = true, silent = true })
-	vim.keymap.set("n", "<leader>p", manager.prompt, { desc = "[P]rompt", noremap = true, silent = true })
+	-- Set up global keymaps
+	for action, keymap in pairs(M.config.keymaps) do
+		if action ~= "buffer" then
+			vim.keymap.set("n", keymap.key, manager[action], {
+				desc = keymap.desc,
+				noremap = true,
+				silent = true,
+			})
+		end
+	end
 end
 
 return M
